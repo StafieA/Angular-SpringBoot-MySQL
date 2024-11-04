da702e2d8a3cec8b278c70c0eb9f3cfcab86717b
Andrei Stafie <andreistafie@gmail.com>
2024-11-04 13:29:40 +0200

    Add combined .gitignore for Angular and Spring Boot projects

---
+++ SpringBootShoppingApp/src/main/java/com/Stafie/backendStafie/BackendStafieApplication.java
@@ -0,0 +1,13 @@
+package com.Stafie.backendStafie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStafieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStafieApplication.class, args);
+	}
+
+}
